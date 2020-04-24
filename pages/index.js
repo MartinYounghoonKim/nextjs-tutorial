6e882cf864d66439c11d7f24278ba8acf1945363
martinyounghoonkim <hoons0131@gmail.com>
2020-04-24 15:11:02 +0900

    배포 테스트

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Index = () => (
   <div>
-    <h1>Hello, World</h1>
+    <h1>Hello, Martin</h1>
     <ul>
       <PostLink id="nextjs-1" title="Hello NextJS 111 테스트"/>
       <PostLink id="nextjs-2" title="Hello NextJS 222 테스트"/>
